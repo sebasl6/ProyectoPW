cf42e76a8f94dcb79891d48eacb44ba71dae3072
Cristian CD <143566189+kriskoCD@users.noreply.github.com>
2023-10-13 18:29:50 -0500

    Update contactoapiArchivo.js

--- ProyectoBiblioteca/src/pages/api/contactoapiArchivo.js
+++ ProyectoBiblioteca/src/pages/api/contactoapiArchivo.js
@@ -1,9 +1,9 @@
 import fsPromises from 'fs/promises'
 export default async function otroEjemplito(req, res){
     if( req.method !== 'POST'){
+        //las peticiones van con GET cuando escribo directamente el URL
         res.status(405).send({message: "Solo funciona con POST !!"})
     }else if(req.method === 'POST'){
-        
         //Leer los valores recibidos
         console.log("BODY:" , req.body)
         const body = JSON.stringify(req.body).replace(" ' ", ' " ')
