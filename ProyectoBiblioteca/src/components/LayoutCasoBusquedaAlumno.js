505255e50efb2dd16366fa7426e8f8435cf2e53f
Cristian CD <143566189+kriskoCD@users.noreply.github.com>
2023-10-13 01:54:02 -0500

    Create LayoutCasoBusquedaAlumno.js

---
+++ ProyectoBiblioteca/src/components/LayoutCasoBusquedaAlumno.js
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+export default props => (
+  <>
+    <div className="header">
+      <div className="left">
+        <img src="/menu_24px.png" alt="Imagen Izquierda" />
+      </div>
+      <div className="center">
+        <h1>Bibliotecas</h1>
+      </div>
+      <div className="right">
+        <img src="/account_circle_filled_24px.png" alt="Imagen Derecha" />
+      </div>
+    </div>
+    <div className="content-wrapper">
+      <main className="main-content-2">
+        {props.content}
+      </main>
+    </div>
+  </>
+);
