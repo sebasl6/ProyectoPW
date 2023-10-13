e3d7d9c3eb566716b364b00b37e40b5d327a3031
FZuasnabar <139509842+FZuasnabar@users.noreply.github.com>
2023-10-13 01:49:28 -0500

    Add files via upload

---
+++ ProyectoBiblioteca/src/api/actualizarRegistros/leerUsuario.js
@@ -0,0 +1,21 @@
+import fsPromises from 'fs/promises';
+import path from 'path';
+import process from 'process';
+
+
+export default async function leerUsuario(req, res) {
+
+    let filepath = '././usuario.json';
+    let ruta = path.join(process.cwd(), filepath);
+
+    try {
+        const data = await fsPromises.readFile(ruta);
+        console.log(data);
+
+        res.status(200).json(JSON.parse(data));
+    } catch (error) {
+
+        console.log("ocurrio un error");
+    }
+
+}
